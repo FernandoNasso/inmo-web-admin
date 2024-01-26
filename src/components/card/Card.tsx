b43538b79110670ad925ac0582ff4d4cc43138ef
FernandoNasso <fernando.nasso@gmail.com>
2024-01-26 17:11:43 -0300

    agregué boton edit en las card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Property } from '../../interfaces/propertyTypes';
-import styles from './Card.module.css'; // Importa el archivo de estilos
+import styles from './Card.module.css'; 
+import { useNavigate } from 'react-router-dom';
+
 
 interface CardProps {
   property: Property;
 }
 
 const Card: React.FC<CardProps> = ({ property }) => {
+  const navigate = useNavigate();
+
+  const handleEditClick = () => {
+    navigate(`/edit/${property._id}`);
+  };
+
   return (
-    <div className={styles.card}> {/* Aplica la clase de estilo */}
+    <div className={styles.card}>
       <h2>{property.title}</h2>
       <p>{property.description}</p>
       <p>{property.price}</p>
+      <button onClick={handleEditClick}>Editar</button>
     </div>
   );
 };
